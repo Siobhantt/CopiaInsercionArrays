d1e8945dd6d8e36c9d1f09c95e8268c22183e741
lcardozo <lcardozo@106-25.info2>
2023-01-13 13:01:46 +0100

    (:

--- CopiaInsercionElementArrays/src/ejercicios/Ej4.java
+++ CopiaInsercionElementArrays/src/ejercicios/Ej4.java
@@ -3,18 +3,25 @@ package ejercicios;
 import java.util.Scanner;
 
 public class Ej4 {
-/*Escribir un programa que lea 10 valores enteros por teclado y los almacene en una tabla (tablaEnteros). 
- * A continuación, debes construir una tabla que contenga los mismos elementos que tablaEnteros pero sin repetir. 
- * Usa el algoritmo de búsqueda explicado en los apuntes.*/
+	/*
+	 * Escribir un programa que lea 10 valores enteros por teclado y los almacene en
+	 * una tabla (tablaEnteros). A continuación, debes construir una tabla que
+	 * contenga los mismos elementos que tablaEnteros pero sin repetir. Usa el
+	 * algoritmo de búsqueda explicado en los apuntes.
+	 */
 	public static void main(String[] args) {
-		
-		int tablaEnteros[] =  new int[10];
-		Scanner lee = new Scanner(System.in);
-		
-		for(int i=0; i<tablaEnteros.length;i++) {
-			tablaEnteros[i]=lee.nextInt();
-		}
 
-	}
+		int tablaEnteros[] = new int[10];
+		int tablaDesordenada[] = new int[tablaEnteros.length];
+		Scanner lee = new Scanner(System.in);
+
+		System.out.println("Introduzca numeros separados por enter: ");
+		for (int i = 0; i < tablaEnteros.length; i++) {// Aqui estoy llenando la tabla
+			tablaEnteros[i] = lee.nextInt();
+		}
+		
+
+		
+	}//Fin del MAIN
 
 }
