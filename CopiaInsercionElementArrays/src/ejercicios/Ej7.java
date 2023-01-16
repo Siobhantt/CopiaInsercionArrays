313bd783a7b75eded1d020f58bba7c1a467e4810
lcardozo <lcardozo@106-25.info2>
2023-01-16 10:07:49 +0100

    no entiendo nada

--- CopiaInsercionElementArrays/src/ejercicios/Ej7.java
+++ CopiaInsercionElementArrays/src/ejercicios/Ej7.java
@@ -8,13 +8,20 @@ public class Ej7 {
 	 * Crea un programa que cree un array de 10 enteros y luego muestre el siguiente
 	 * menú con distintas opciones: Mostrar valores. Introducir valor. Salir.
 	 */
+	/*La opción ‘a’ mostrará todos los valores por pantalla. 
+	 * La opción ‘b’ pedirá un valor V y una posición P, luego escribirá V en la posición P del array. 
+	 * El menú se repetirá indefinidamente hasta que el usuario elija la opción ‘c’ que terminará el programa*/
 	public static void main(String[] args) {
+		int valor;
+		int posicion;
 		int numUser;
 		int tabla[] = new int[10];
 		int opcionUser;
 		boolean salida = true;// Variable para administrar la opcion de salida
 		Scanner lee = new Scanner(System.in);
 
+	
+		
 		do {
 		System.out.println("Elija la opcion que prefiera: ");
 		System.out.println("1: Mostrar valores" + "\n" + "2: Introducir valor" + "\n" + "3: Salir");
@@ -30,7 +37,10 @@ public class Ej7 {
 				break;
 
 			case 2:
-				System.out.println("Por favor introduzca los valores que desee.");
+				System.out.println("Por favor introduzca una posicion: ");
+				posicion = lee.nextInt();
+				System.out.println("Por favor introduzca un valor: ");
+				valor=lee.nextInt();
 				for (int i = 0; i < tabla.length; i++) {
 					numUser = lee.nextInt();
 				} // fin for
